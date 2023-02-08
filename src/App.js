d95ba5798f6ec25ef788e4f41b530ff13be84aa5
rankshow <uwaomaobinna20@gmail.com>
2023-02-08 23:56:48 +0100

    initialize files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Home pokemon</h1>
+      <div className='container'>
+         <h1>Home pokemon</h1>
+         <input type='text' />
+         <button></button>
+      </div>
     </div>
   );
 }
