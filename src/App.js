75313c423fbdce81cb34adb8ae9f22717a6b10b7
rankshow <uwaomaobinna20@gmail.com>
2023-02-09 19:39:48 +0100

    added axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useState } from "react";
+import Axios from "axios";
 import './App.css';
 
-function App() {
+
+
+const App = () => {
+  //Setting the Pokemon state
+  const [pokemonName, setPokkemonName] = useState("");
+
+  // Handling the search for pokemon event and consuming the pokemon Api
+  const searchPokemonName = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
+      console.log(response)
+    })
+  };
+
+
   return (
     <div className="App">
       <div className='container'>
          <h1>Home pokemon</h1>
-         <input type='text' />
-         <button></button>
+         <input type={pokemonName}  onChange={(e) => setPokkemonName(e.target.value)} />
+         <button onClick={searchPokemonName}>Search pokemon</button>
       </div>
     </div>
   );
