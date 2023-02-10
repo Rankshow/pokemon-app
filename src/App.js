db36ed0f5c2bad043f642d8f39014e9ac4d732b1
rankshow <uwaomaobinna20@gmail.com>
2023-02-10 10:25:24 +0100

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import Axios from "axios";
+import axios from "axios";
 import "./App.css";
 
 const App = () => {
   //Setting the Pokemon state
   const [pokemonName, setPokemonName] = useState("");
-  const [pokemonChosen, setPokemonChosen] = useState(false)
+  const [pokemonChosen, setPokemonChosen] = useState(false);
   const [pokemon, setPokemon] = useState({
     name: "",
     species: "",
@@ -18,8 +18,9 @@ const App = () => {
 
   // Handling the search for pokemon event and consuming the pokemon Api
   const searchPokemonName = () => {
-    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
-      (response) => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => {
         setPokemon({
           name: pokemonName,
           species: response.data.name,
@@ -27,27 +28,37 @@ const App = () => {
           hp: response.data.stats[0].base_stat,
           attack: response.data.stats[1].base_stat,
           defense: response.data.stats[2].base_stat,
-          types: response.data.stats[0].type.name,
+          type: response.data.types[0].type.name,
         });
-        setPokemonChosen(true)
-      }
-    );
+        setPokemonChosen(true);
+      });
   };
 
   return (
-    <div className="App">
-      <div className="container">
-        <h1>Home pokemon</h1>
-        <input
-          type={pokemonName}
-          onChange={(e) => setPokemonName(e.target.value)}
-        />
-        <button onClick={searchPokemonName}>Search pokemon</button>
+    <>
+      <div className="App">
+        <div className="container">
+          <h1>Home pokemon</h1>
+          <input
+            type={pokemonName}
+            onChange={(e) => setPokemonName(e.target.value)}
+          />
+          <button onClick={searchPokemonName}>Search pokemon</button>
+        </div>
+        <div className="displaySection">
+          {!pokemonChosen ? (
+            <h1>Please chose a pokemon</h1>
+          ) : (
+            <>
+              <div className="subSection">
+                <h1>{pokemon.name}</h1>
+                <img src={pokemon.img} />
+              </div>
+            </>
+          )}
+        </div>
       </div>
-      <div className="displaySection">
-        {!pokemonChosen ? (<h2>Please check pokemon chosen</h2>) : (<h1>{pokemonName}</h1>)}
-      </div>
-    </div>
+    </>
   );
 };
 
